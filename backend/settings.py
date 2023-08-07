1ebc6682cfd96d6bc8b332cd454a4d99b6ba7501
FREDERICO23 <adhingafredrick@gmail.com>
2023-08-07 17:57:33 +0300

    - Fix a bug in with heroku deployment

--- backend/settings.py
+++ backend/settings.py
@@ -216,3 +216,4 @@ MEDIA_URL = '/media/'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 django_heroku.settings(locals())
+del DATABASES['default']['OPTIONS']['sslmode'] 
