90119c4f1d9e8fcde6aa07e60e184bb31509f319
FREDERICO23 <adhingafredrick@gmail.com>
2024-01-26 13:17:16 +0300

    wsgi update

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -2,8 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-settings_module = 'backend.production' if 'PRODUCTION' in os.environ else 'backend.settings'
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
 application = get_wsgi_application()
